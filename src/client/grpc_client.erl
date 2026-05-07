20fd453a5ddf18eec4d71d4eee1ebf9f2c259f7a
Thales Macedo Garitezi <16166434+thalesmg@users.noreply.github.com>
2026-05-07 11:32:13 -0300

    fix(client): fix typespec

--- src/client/grpc_client.erl
+++ src/client/grpc_client.erl
@@ -87,6 +87,9 @@
          %% The grpc-encoding method
          %% Default is identity
          , encoding => grpc_frame:encoding()
+         %% Content type to use in grpc header.
+         %% Default is `application/grpc+json`
+         , content_type => binary()
          %% The timeout to receive the response of request. The clock starts
          %% ticking when the request is sent
          %%
